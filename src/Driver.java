ba6d46f57d3429413a081c0406f10bed2081d1b8
Goutam <goutam97kundu@gmail.com>
2021-12-12 22:28:24 +0530

    Bill Builder added

--- src/Driver.java
+++ src/Driver.java
@@ -1,24 +1,43 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import exceptions.GroupNameExistsException;
+import exceptions.GroupNotFoundException;
 import exceptions.UserNameExistsException;
 import exceptions.UserNotFoundException;
+import models.Bill;
+import models.BillBuilder;
+import services.BillServiceImpl;
 import services.GroupService;
 import services.GroupServiceImpl;
 import services.UserService;
 import services.UserServiceImpl;
 
 public class Driver {
-	public static void main(String args[]) throws GroupNameExistsException, UserNotFoundException, UserNameExistsException {
+	public static void main(String args[]) throws GroupNameExistsException, UserNotFoundException, UserNameExistsException, GroupNotFoundException {
 		UserService userService = new UserServiceImpl();
-		
+
 		userService.createUser("Goutam");
 		userService.createUser("Piyush");
 		userService.createUser("Kaushal");
-		
+
 		GroupService groupService = new GroupServiceImpl();
 		groupService.createGroup("Blr flatmates", Arrays.asList("Goutam", "Piyush", "Kaushal"));
+
+		Map<String, Double> paidBy = new HashMap<>(), ownedBy = new HashMap<>();
+		paidBy.put("Goutam", 300.0);
+
+		ownedBy.put("Goutam", 100.0);
+		ownedBy.put("Piyush", 100.0);
+		ownedBy.put("Kaushal", 100.0);
+
+		Bill bill = new BillBuilder().setName("Rent").setAmount(300.0).setPaidBy(paidBy).setOwnedBy(ownedBy).getBill();
+
+		new BillServiceImpl().addBillToGroup(bill, "Blr flatmates");
 		
-		System.out.println(userService.getUserBalance("Goutam"));
+		System.out.println("Goutam: " + userService.getUserBalance("Goutam"));
+		System.out.println("Piyush: " + userService.getUserBalance("Piyush"));
+		System.out.println("Kaushal: " + userService.getUserBalance("Kaushal"));
 	}
 }
